30a717dfdd9b953c3246315b211fb5ecb3bff7ba
Mateusz Rumian <mrumian@sumologic.com>
2021-09-29 20:14:43 +0200

    Update deps, add resource

--- javascript/tracer.js
+++ javascript/tracer.js
@@ -1,36 +1,41 @@
 'use strict';
 
-const opentelemetry = require("@opentelemetry/api");
+const opentelemetry = require('@opentelemetry/api');
 
-const { NodeTracerProvider } = require('@opentelemetry/node');
-const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector');
+const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
+const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector-proto');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+const { Resource } = require('@opentelemetry/resources');
 
 // Instrumentations
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
 const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
 
 // Add ConsoleSpanExporter e.g { BatchSpanProcessor, ConsoleSpanExporter }  - debug purposes
-const { BatchSpanProcessor } = require("@opentelemetry/tracing");
+const { BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 
 // Uncomment in case of additional debug logging
 //const { diag, DiagConsoleLogger, DiagLogLevel } = opentelemetry;
 //diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
 module.exports = () => {
- const provider = new NodeTracerProvider();
+ const resources = new Resource({
+  'service.name': 'YOUR_SERVICE_NAME',
+  'application': 'YOUR_APPLICATION_NAME',
+  //'ANY_OTHER_ATTRIBUTE_KEY': 'ANY_OTHER_ATTRIBUTE_VALUE',
+ });
 
- const exporterOptions = {
-  serviceName: "js-test",
+ const provider = new NodeTracerProvider({ resource: resources});
+
+ const exporterOptions = {
   url: 'http://otelcol:55681/v1/traces',
-  headers: {}
  }
 
  // Uncomment in case of enabled ConsoleExporter
- //let consoleExporter = new ConsoleSpanExporter();
+ //const consoleExporter = new ConsoleSpanExporter();
  //provider.addSpanProcessor(new BatchSpanProcessor(consoleExporter));
 
- let exporter = new CollectorTraceExporter(exporterOptions);
+ const exporter = new CollectorTraceExporter(exporterOptions);
  provider.addSpanProcessor(new BatchSpanProcessor(exporter));
  provider.register();
 
