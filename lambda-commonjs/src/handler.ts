1a720f58e0f45218061cd81c37f3bbf2955922af
Mateusz Rumian <mrumian@sumologic.com>
2024-08-01 12:44:47 +0200

    chore: add example lambda commonjs

---
+++ lambda-commonjs/src/handler.ts
@@ -0,0 +1,12 @@
+import { S3 } from 'aws-sdk';
+
+export const listS3Buckets = async (): Promise<string[]> => {
+  const s3 = new S3();
+  try {
+    const response = await s3.listBuckets().promise();
+    return response.Buckets ? response.Buckets.map(bucket => bucket.Name).filter((name): name is string => !!name) : [];
+  } catch (error) {
+    console.error('Error listing S3 buckets:', error);
+    throw error;
+  }
+};
---
+++ lambda-commonjs/src/index.ts
@@ -0,0 +1,30 @@
+import { listS3Buckets } from './handler';
+
+const handler = async (event: any): Promise<any> => {
+  try {
+    const buckets = await listS3Buckets();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(buckets),
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: 'Error listing S3 buckets',
+          error: error.message,
+        }),
+      };
+    } else {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: 'An unknown error occurred',
+        }),
+      };
+    }
+  }
+};
+
+module.exports.handler = handler;
